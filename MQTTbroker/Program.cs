be4101343fdb5b1b0fedfa7077143c4bea538978
nappo <niccolo.nappini@estrobit.com>
2019-10-17 15:10:49 +0200

    first commit

---
+++ MQTTbroker/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using MQTTbroker;
+using MQTTnet;
+using MQTTnet.Server;
+using System.Threading.Tasks;
+using MQTTnet.Protocol;
+using MQTTnet.Client.Receiving;
+
+namespace MQTTbroker
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var mqttServer = new MqttFactory().CreateMqttServer();
+            var optionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionValidator(c =>
+                {
+                    if (c.ClientId.Length < 10)
+                    {
+                        c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedIdentifierRejected;
+                        return;
+                    }
+
+                    if (c.Username != "asd")
+                    {
+                        c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+                        return;
+                    }
+
+                    if (c.Password != "123Stella")
+                    {
+                        c.ReturnCode = MqttConnectReturnCode.ConnectionRefusedBadUsernameOrPassword;
+                        return;
+                    }
+
+                    c.ReturnCode = MqttConnectReturnCode.ConnectionAccepted;
+                });
+            await mqttServer.StartAsync(optionsBuilder.Build());
+            mqttServer.ApplicationMessageReceivedHandler = new MessageHandler();
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+            await mqttServer.PublishAsync("set-on");
+            await mqttServer.PublishAsync("set-on");
+
+            await mqttServer.StopAsync();
+        }
+
+       
+    }
+
+    public class MessageHandler : IMqttApplicationMessageReceivedHandler
+    {
+        public Task HandleApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs eventArgs)
+        {
+            var tt = eventArgs.ApplicationMessage.Payload;
+
+            string result = System.Text.Encoding.UTF8.GetString(tt);
+
+
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ MQTTclient/Program.cs
@@ -0,0 +1,27 @@
+﻿using MQTTnet;
+using MQTTnet.Client.Options;
+using System;
+using System.Threading;
+
+namespace MQTTclient
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Create a new MQTT client.
+            var factory = new MqttFactory();
+            var mqttClient = factory.CreateMqttClient();
+            var options = new MqttClientOptionsBuilder()
+                                .WithClientId("Client1")
+                                .WithTcpServer("127.0.0.1")
+                                .WithCredentials("bud", "%spencer%")
+                                .WithTls()
+                                .WithCleanSession()
+                                .Build();
+
+            await mqttClient.ConnectAsync(options, CancellationToken.None);
+        }
+    }
+}
